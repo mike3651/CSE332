d80794c28182e31180bccb2d8df322d67c35e190
Michael <anthony_michael_wilson@yahoo.com>
2016-05-02 15:14:37 -0700

    added the start of the algorithm GUI

---
+++ CSE/332/.metadata/.plugins/org.eclipse.core.resources/.history/54/c0ea7921b2100016107b9af58f243d56
@@ -0,0 +1,30 @@
+package Graphical_Interface;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+//Michael Wilson
+//CSE Student project
+//Start Date: May 2, 2016
+//End Date  : 
+
+/**
+ * This class deals with displaying the algorithm graphics.
+ * 
+ * @author Michael Wilson
+ * @version 1.0
+ */
+public class AlgorithmPanel extends JPanel {
+
+	/** Default serial number.*/
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Constructs the panel.
+	 */
+	public AlgorithmPanel() {
+		setBackground(Color.RED);
+	}
+	
+}
---
+++ CSE/332/Algorithm GUI/src/Graphical_Interface/Start.java
@@ -0,0 +1,44 @@
+package Graphical_Interface;
+// Michael Wilson
+// CSE Student project
+// Start Date: May 2, 2016
+// End Date  : 
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+/**
+ * This class kicks off the whole program.
+ * 
+ * @author Michael Wilson
+ * @version 1.0
+ */
+public class Start {
+	/**
+	 * Private constructor that prevents the compiler 
+	 * from making an unnecessary object. 
+	 */
+	private Start() { }
+	
+	/**
+	 * This is the main method that kicks off the program.
+	 * 
+	 * @param theArgs Arguments from the command line.
+	 */
+	public static void main(final String[] theArgs) {
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+			        UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+			    } 
+			    catch (Exception e) {
+			    }
+				new AFrame();
+			}
+			
+		});
+
+	}
+}
